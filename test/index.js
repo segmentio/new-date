276af3d3d23eab41b2d895227aaf1c80dc58fca4
Nathan Houle <nathan@nathanhoule.com>
2016-05-11 16:21:14 -0700

    Make test asserts log better info

--- test/index.js
+++ test/index.js
@@ -7,7 +7,7 @@ describe('new-date', function() {
   describe('dates', function() {
     it('new Date', function() {
       var date = new Date();
-      assert(newDate(date) === date);
+      assert.strictEqual(newDate(date), date);
     });
   });
 
@@ -15,13 +15,13 @@ describe('new-date', function() {
     it('1363288923637 (milliseconds)', function() {
       var millis = 1363288923637;
       var date = newDate(millis);
-      assert(date.getTime() === millis);
+      assert.strictEqual(date.getTime(), millis);
     });
 
     it('1363288923 (seconds)', function() {
       var seconds = 1363288923;
       var date = newDate(seconds);
-      assert(date.getTime() === seconds * 1000);
+      assert.strictEqual(date.getTime(), seconds * 1000);
     });
   });
 
@@ -33,50 +33,49 @@ describe('new-date', function() {
 
     it('"Wed, 09 Aug 1995"', function() {
       var date = newDate('Wed, 09 Aug 1995');
-      assert(date.getTime() === 807951600000);
+      assert.strictEqual(date.getTime(), 807951600000);
     });
 
     it('"Wed, 09 Aug 1995 04:10:23 GMT"', function() {
       var date = newDate('Wed, 09 Aug 1995 04:10:23 GMT');
-      assert(date.getTime() === 807941423000);
+      assert.strictEqual(date.getTime(), 807941423000);
     });
 
     it('"2013-05-31" (MySQL)', function() {
       var date = newDate('2013-05-31');
-      assert(date.getTime() === 1369958400000);
+      assert.strictEqual(date.getTime(), 1369958400000);
     });
 
     it('"2013-05-31 00:24:47" (MySQL)', function() {
       var date = newDate('2013-05-31 00:24:47');
-      assert(date.getTime() === 1369985087000);
+      assert.strictEqual(date.getTime(), 1369985087000);
     });
 
     it('"2013-05-31T00:24:47" (ISO)', function() {
       var date = newDate('2013-05-31T00:24:47');
-      assert(date.getTime() === 1369959887000);
+      assert.strictEqual(date.getTime(), 1369959887000);
     });
 
     it('"2013-09-04T01:06:46.983-0700" (ISO)', function() {
       var date = newDate('2013-09-04T01:06:46.983-0700');
-      assert(date.getTime() === 1378282006983);
+      assert.strictEqual(date.getTime(), 1378282006983);
     });
 
     it('"2013-09-04T01:06:46.983Z" (ISO)', function() {
       var date = newDate('2013-09-04T01:06:46.983Z');
-      assert(date.getTime() === 1378256806983);
+      assert.strictEqual(date.getTime(), 1378256806983);
     });
 
     it('"1363288923637" (milliseconds)', function() {
       var millis = '1363288923637';
       var date = newDate(millis);
-      assert(date.getTime() === 1363288923637);
+      assert.strictEqual(date.getTime(), 1363288923637);
     });
 
     it('"1363288923" (seconds)', function() {
       var seconds = '1363288923';
       var date = newDate(seconds);
-      assert(date.getTime() === 1363288923 * 1000);
+      assert.strictEqual(date.getTime(), 1363288923 * 1000);
     });
   });
-
 });
